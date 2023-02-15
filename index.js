06d8ce88cf3a8b9a157166b739ec758e0bfbd2d1
Red <rachel.ik.87@gmail.com>
2023-02-15 14:14:25 +0800

    fine tuning

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ function submit_rating_handler() {
 }
 
 function rating_click() {
-    document.getElementById('rating-score').innerHTML = event.target.innerHTML;
+    let x = event.target.innerHTML;
+    document.getElementById('rating-score').innerHTML = x;
 
     ratingBtns = document.getElementsByClassName("rating-button");
     for (var i = 0; i < ratingBtns.length; i++) {
-        ratingBtns[i].style.background = 'hsl(213, 19%, 18%)';
-        ratingBtns[i].style.color = '#7c8798';
+
+        if (ratingBtns[i].innerHTML != x) {
+            ratingBtns[i].style.background = 'hsl(213, 19%, 18%)';
+            ratingBtns[i].style.color = '#7c8798';
+        }
+        else {
+            event.target.style.background = 'hsl(25, 97%, 53%)';
+            event.target.style.color = 'white';
+        }
     }
-
-    event.target.style.background = 'hsl(25, 97%, 53%)';
-    event.target.style.color = 'white';
-
 }
