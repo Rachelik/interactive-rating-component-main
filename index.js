51f1685fccc508e238411de61ccdc01ea4a80f39
Red <rachel.ik.87@gmail.com>
2023-02-15 14:04:53 +0800

    change font-size to rem, touch up interaction and update footer

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ function rating_click() {
     ratingBtns = document.getElementsByClassName("rating-button");
     for (var i = 0; i < ratingBtns.length; i++) {
         ratingBtns[i].style.background = 'hsl(213, 19%, 18%)';
+        ratingBtns[i].style.color = '#7c8798';
     }
 
     event.target.style.background = 'hsl(25, 97%, 53%)';
+    event.target.style.color = 'white';
 
 }
